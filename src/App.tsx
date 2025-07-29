2cfc22f395978e878c529ab39acde02ad089e91f
kuldeep-atharva <kuldeepsinh@atharvasystem.com>
2025-07-29 20:08:59 +0530

    router flag

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,12 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <Router>
+      <Router
+        future={{
+          v7_startTransition: true,
+          v7_relativeSplatPath: true,
+        }}
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/wayfinder" element={<Wayfinder />} />
